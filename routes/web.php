7cc35a5b5d87dd8ccef3fbb7a86d2bed75240626
rachid <ouchaikhrachid3@gmail.com>
2023-09-29 09:36:43 +0000

    task management

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,7 @@
 
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\TasksController;
-use Illuminate\Foundation\Application;
+
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
@@ -17,14 +17,7 @@ use Inertia\Inertia;
 |
 */
 
-// Route::get('/', function () {
-//     return Inertia::render('Login', [
-//         'canLogin' => Route::has('login'),
-//         'canRegister' => Route::has('register'),
-//         'laravelVersion' => Application::VERSION,
-//         'phpVersion' => PHP_VERSION,
-//     ]);
-// });
+
 Route::redirect('/', '/tasks');
 Route::get('/dashboard', function () {
     return Inertia::render('Dashboard');
